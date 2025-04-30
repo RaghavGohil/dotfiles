bba48cea50d35cca978e308a09d2626061e38fc8
Raghav Gohil <raghavgohil2004@gmail.com>
2025-04-30 21:21:16 +0530

    fixed file struct, time to fix my broken distro (lmao)

--- nvim/.config/nvim/after/plugin/telescope.lua
+++
@@ -1,6 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- hit pf and then escape to move with jk.
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
